e11fed30fac8c9789aa9ed8447eef464ee265fc1
pranavgupta6 <gpranav859@gmail.com>
2026-01-30 12:39:15 +0530

    ai-service-setup

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,26 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintanable code. You write simple Next.js & React code snippets.",
+      model: openai({
+         model : "gpt-4o-mini",
+         apiKey : process.env.AI_PIPE_TOKEN,
+         baseUrl : process.env.AI_PIPE_BASE_URL
+
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    return { output }
   },
 );
