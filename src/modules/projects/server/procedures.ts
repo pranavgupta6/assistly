cfa12143d17a1e7f01d7f190fe540ad1d3e1f1cf
pranavgupta6 <gpranav859@gmail.com>
2026-02-18 23:58:54 +0530

    billing

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -4,8 +4,10 @@ import { generateSlug } from "random-word-slugs";
 import { protectedProcedure, createTRPCRouter} from '@/trpc/init';
 import { z } from 'zod';
 import { TRPCError } from '@trpc/server';
+import { consumeCredits } from '@/lib/usage';
 
 export const projectsRouter = createTRPCRouter({
+
     getOne : protectedProcedure
     .input (z.object({
         id : z.string().min(1, { message: "ID is required" }),
@@ -27,6 +29,7 @@ export const projectsRouter = createTRPCRouter({
 
         return existingProject;
     }),
+
     getMany : protectedProcedure
     .query(async ({ ctx }) => {
         const projects = await prisma.project.findMany({
@@ -50,6 +53,23 @@ export const projectsRouter = createTRPCRouter({
         }),
     )
     .mutation(async ({ input, ctx }) => {
+
+        try{
+            await consumeCredits();
+        } catch(error) {
+            if (error instanceof Error){
+                throw new TRPCError({
+                    code : "BAD_REQUEST",
+                    message : "Something went wrong",
+                });
+            } else {
+                throw new TRPCError({
+                    code : "TOO_MANY_REQUESTS",
+                    message : "You have run out of credits.",
+                });
+            }
+        }
+
         const createdProject = await prisma.project.create({
             data : {
                 userId : ctx.auth.userId,
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -12,6 +12,7 @@ import { EyeIcon, CodeIcon, CrownIcon, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { UserControl } from "@/components/user-control";
+import { useAuth } from "@clerk/nextjs";
 
 
 
@@ -21,6 +22,8 @@ interface Props {
 
 export const ProjectView = ({ projectId }: Props) => {
 
+    const { has } = useAuth();
+    const hasProAccess = has?.({ plan : "pro" });
     const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
     const [tabState , setTabState] = useState<"preview" | "code">("preview");
 
@@ -64,11 +67,13 @@ export const ProjectView = ({ projectId }: Props) => {
                                 </TabsTrigger>
                             </TabsList>
                             <div className="ml-auto flex items-center gap-x-2">
+                                {!hasProAccess && (
                                 <Button asChild size="sm" variant="tertiary">
                                     <Link href="/pricing">
                                         <CrownIcon /> Upgrade
                                     </Link>
                                 </Button>
+                                )}
                                 <UserControl />
                             </div>
                         </div>
